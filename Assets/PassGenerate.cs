99fd6603df5728854f80286ba0d67ddf5f214c02
yuito imamura <yui7.mario@gmail.com>
2018-05-15 20:19:00 +0900

    ボタンを置いてパターンの配列の枠だけ作りました

---
+++ Assets/PassGenerate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PassGenerate : MonoBehaviour {
+	public static int[] basicPass = new int[9]{1,2,3,4,5,6,7,8,9};
+	public static int[] pass = new int[9];
+	// Use this for initialization
+	void Start () {
+		pass = basicPass.OrderBy(i => Guid.NewGuid()).ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
